896a364046a261aaca33c87321222d0efcb48b66
Marcin Ciborowski <ciborowskim@outlook.com>
2016-06-29 21:45:29 +0200

    Show some data from tooltips.

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
-//var highcharts_container_svg = null;
-var start = null;
-var end = null;
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  var body = document.body;
+  var docEl = document.documentElement;
+
+  var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+  var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+  var clientTop = docEl.clientTop || body.clientTop || 0;
+  var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+  var top = box.top + scrollTop - clientTop;
+  var left = box.left + scrollLeft - clientLeft;
+
+  return { top: Math.round(top), left: Math.round(left) };
+}
+
+function createElement(tag, id) {
+  var newElement = document.createElement(tag);
+  if (id)
+    newElement.id = id;
+  return newElement;
+}
+
+function createSpeed() {
+  var highchartsDiv = document.getElementsByClassName("highcharts-container")[0];
+  var coords = getCoords(highchartsDiv);
+  var speed = document.createElement("div");
+  speed.id = "speed";
+  speed.style = "position: absolute; top: " + coords.top + "px; left: " + (coords.left + highchartsDiv.clientWidth) +
+    "px; border-left: 6px solid green;";
+  speed.insertAdjacentElement("beforeEnd", createElement("div", "speedp"));
+  speed.insertAdjacentElement("beforeEnd", createElement("div", "timep"));
+  speed.insertAdjacentElement("beforeEnd", createElement("div", "distp"));
+  return speed;
+}
+
+function showData(avgspeed, time, distance) {
+  var speed = document.getElementById("speed");
+  if (speed == null) {
+    document.body.insertAdjacentElement("afterBegin", createSpeed());
+  }
+
+  document.getElementById("speedp").innerHTML = "Speed: " + avgspeed;
+  document.getElementById("timep").innerHTML = "Time: " + time;
+  document.getElementById("distp").innerHTML = "Distance: " + distance;
+}
+
+function hideData() {
+
+}
 
 function getData() {
   var highchartsContainerSvg = document.getElementsByClassName("highcharts-container")[0];
@@ -12,22 +61,24 @@ function getData() {
   return data;
 }
 
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   if (request.newWorkout)
-
-//     sendResponse();
-// });
-
+// Data from endomondo toopltip: [duration,distance,speed,altitude]
+var startData = null;
+var mousemoved = false;
 document.onmousedown = function (e) {
-  if (e.srcElement.nodeName == "path" || e.srcElement.nodeName == "rect") {
-    var data = getData();
-    console.log(data);
+  mousemoved = false;
+  if (e.path.find(function (e) { return e.nodeName == "svg" })) {
+    startData = getData();
   }
+}
+document.onmousemove = function (e) {
+  mousemoved = true;
 }
 
 document.onmouseup = function (e) {
-  if (e.srcElement.nodeName == "rect") {
-    var data = getData();
-    console.log(data);
+  if (e.path.find(function (e) { return e.nodeName == "svg" }) && mousemoved) {
+    mousemoved = false;
+    var endData = getData();
+    showData(startData[2] + " " + endData[2], startData[0] + " " + endData[0], startData[1] + " " + endData[1]);
+    
   }
 }
--- background.js
+++ background.js
@@ -11,3 +11,18 @@
 //       });
 //   }
 // });
+
+// Data from endomondo toopltip: [duration,distance,speed,altitude]
+// var lastData = null;
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if ("start" in request &&
+//     "end" in request &&
+//     request.start.length == 4 &&
+//     request.end.length == 4) {
+//       console.log(request);
+//       sendResponse("OK");
+//   }
+//   else
+//     sendResponse("Bad request.");
+// })
