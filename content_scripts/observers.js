5737f121c7a2c28b4923334b4b1edddd710d97f9
Marcin Ciborowski <ciborowskim@outlook.com>
2016-07-05 18:11:22 +0200

    Hide data when "reset zoom" button is pressed.

--- content_scripts/observers.js
+++ content_scripts/observers.js
@@ -1,14 +1,28 @@
 /// </// <reference path="data_functions.js" />
 
-// function getSvg(){
-//   return document.getElementsByClassName("highcharts-container")[0].firstChild;
-// }
 function getAnalysisNavtab() {
   return document.getElementsByClassName("nav")[0].getElementsByTagName("li")[0];
 }
 
 var svgObserver = new MutationObserver(function (mutations) {
-  console.log({ svg: mutations });
+  //console.log({ svg: mutations });
+
+  var button = null;
+  for (var i = 0; i < mutations[0].addedNodes.length; i++) {
+    var element = mutations[0].addedNodes[i];
+    var classAtt = element.attributes["class"];
+    if (classAtt && classAtt.value === "highcharts-button") {
+      button = element;
+      break;
+    }
+  }
+
+  if (button === null)
+    return;
+
+  button.addEventListener("click", function (e) {
+    hideData();
+  });
 });
 
 var navtabObserver = new MutationObserver(function (mutations) {
